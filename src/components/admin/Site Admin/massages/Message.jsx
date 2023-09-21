14f9448247dd9604c648c65b2dd759045ce1c4de
Murodxon <94508619+MuradKhan2001@users.noreply.github.com>
2023-09-21 12:24:43 +0500

    21.09.2023

--- src/components/admin/Site Admin/massages/Message.jsx
+++ src/components/admin/Site Admin/massages/Message.jsx
@@ -6,8 +6,9 @@ import axios from "axios";
 const Message = () => {
     let value = useContext(MyContext);
     const [MainList, setMainList] = useState([]);
+    const [menu,setMenu]= useState(false);
 
-    useEffect(() => {
+    const getList = () =>{
         axios.get(`${value.url}dashboard/contactus/`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("token")}`
@@ -20,6 +21,10 @@ const Message = () => {
                 localStorage.removeItem("token");
             }
         });
+    };
+
+    useEffect(() => {
+       getList()
     }, []);
 
     const delMessage = (id) =>{
@@ -39,43 +44,118 @@ const Message = () => {
 
     return <div className="message-container">
 
-        <div className="table-content">
-            <table>
-                <thead>
-                <tr>
-                    <th>№</th>
-                    <th>Ism/Familiya</th>
-                    <th>Tel raqam</th>
-                    <th>Xabar</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                {MainList.map((item, index) => {
-                    return <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>
-                            {item.first_name} <br/>
-                            {item.last_name}
-                        </td>
-                        <td>{item.phone}</td>
-                        <td>
-                            <div className="message-box">
-                                {item.body}
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img onClick={()=>delMessage(item.id)} src="../images/admin/delete.png" alt=""/>
-                            </div>
-                        </td>
-
-                    </tr>
-                })
-                }
-                </tbody>
-            </table>
+        <div className="header-side">
+            <div onClick={()=>setMenu(false)} className={`menu ${!menu ? "active-menu" : ""}`}>
+                Yangi kontakt va takliflar
+            </div>
+            <div onClick={()=>setMenu(true)} className={`menu ${menu ? "active-menu" : ""}`}>
+                O'qilgan kontakt va takliflar
+            </div>
         </div>
+
+        {
+            menu ? <div className="table-content">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>№</th>
+                        <th>Ism/Familiya</th>
+                        <th>Tel raqam</th>
+                        <th>Xabar</th>
+                        <th>O'chirish</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    {MainList.map((item, index) => {
+                        {if (item.read){
+                            return <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    {item.first_name} <br/>
+                                    {item.last_name}
+                                </td>
+                                <td>{item.phone}</td>
+                                <td>
+                                    <div className="message-box">
+                                        {item.body}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <img onClick={()=>delMessage(item.id)} src="../images/admin/delete.png" alt=""/>
+                                    </div>
+                                </td>
+
+                            </tr>
+                        }}
+                    })
+                    }
+                    </tbody>
+                </table>
+            </div> : <div className="table-content">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>№</th>
+                        <th>Ism/Familiya</th>
+                        <th>Tel raqam</th>
+                        <th>Xabar</th>
+                        <th>Tasdiqlash</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    {MainList.map((item, index) => {
+                       if (item.read === false){
+                           return <tr key={index}>
+                               <td>{index + 1}</td>
+                               <td>
+                                   {item.first_name} <br/>
+                                   {item.last_name}
+                               </td>
+                               <td>{item.phone}</td>
+                               <td>
+                                   <div className="message-box">
+                                       {item.body}
+                                   </div>
+                               </td>
+                               <td>
+                                   <div>
+                                       <div>
+                                           {item.read ? <img onClick={() => {
+                                                   axios.post(`${value.url}dashboard/contactus/${item.id}/verify/`, {}, {
+                                                       headers: {
+                                                           "Authorization": `Token ${localStorage.getItem("token")}`
+                                                       }
+                                                   }).then(() => {
+                                                       getList()
+                                                   })
+                                               }} src={`../images/admin/verified.png`} alt=""/> :
+
+                                               <img onClick={() => {
+                                                   axios.post(`${value.url}dashboard/contactus/${item.id}/verify/`, {}, {
+                                                       headers: {
+                                                           "Authorization": `Token ${localStorage.getItem("token")}`
+                                                       }
+                                                   }).then(() => {
+                                                       getList()
+                                                   })
+                                               }} src={`../images/admin/verified1.png`} alt=""/>}
+                                       </div>
+                                   </div>
+                               </td>
+
+                           </tr>
+                       }
+                    })
+                    }
+                    </tbody>
+                </table>
+            </div>
+        }
+
+
     </div>
 };
 
